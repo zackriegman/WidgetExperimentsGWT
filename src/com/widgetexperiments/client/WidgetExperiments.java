97867b90b525426d197065744163f53218fe2885
Isaac Kriegman <zackriegman@gmail.com>
2010-09-08 17:59:35 -0400

    improved layout ... implement a system for disapearing buttons, which doesn't seem to work yet...

--- src/com/widgetexperiments/client/WidgetExperiments.java
+++ src/com/widgetexperiments/client/WidgetExperiments.java
@@ -1,12 +1,20 @@
 package com.widgetexperiments.client;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import com.google.gwt.core.client.EntryPoint;
 
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.dom.client.FocusHandler;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -36,23 +44,23 @@ public class WidgetExperiments implements EntryPoint {
 
 		// Associate the Main panel with the HTML host page.
 		RootPanel.get("widgetExperiments").add(mainPanel);
-		
-		
 
 	}
 
-	private static class Proposition extends TreeItem implements ClickHandler {
-		// TODO: hide buttons when without focus
-		// TODO: limit make text uneditable when not "clarifying"
+	private static class Proposition extends TreeItem implements ClickHandler,
+			KeyPressHandler, FocusHandler {
 		// TODO: synchronize arguments with server
-		// TODO: fix formating ... compress visual space
-		// TODO: return creates new prop?
 
-
+		private static Proposition lastPropositionWithFocus = null;
 		private TextAreaSloppyGrow textArea = new TextAreaSloppyGrow();
-		private Button clarifyButton = new Button("Clarify");
 		private Button proButton = new Button("For");
 		private Button conButton = new Button("Against");
+		private TreeItem parentArgument;
+
+		public Proposition(TreeItem parentArgument) {
+			this();
+			this.parentArgument = parentArgument;
+		}
 
 		public Proposition() {
 			super();
@@ -60,23 +68,26 @@ public class WidgetExperiments implements EntryPoint {
 			HorizontalPanel horizontalPanel = new HorizontalPanel();
 			verticalPanel.add(textArea);
 			verticalPanel.add(horizontalPanel);
-			horizontalPanel.add(clarifyButton);
 			horizontalPanel.add(proButton);
 			horizontalPanel.add(conButton);
 			this.setWidget(verticalPanel);
 
 			proButton.addClickHandler(this);
 			conButton.addClickHandler(this);
-			clarifyButton.addClickHandler(this);
-			
-			setState( true );
+
+			textArea.addKeyPressHandler(this);
+			textArea.addFocusHandler(this);
+			//textArea.addBlurHandler(this);
+			proButton.addFocusHandler( this );
+			//proButton.addBlurHandler( this);
+			conButton.addFocusHandler( this );
+			//conButton.addBlurHandler( this );
+			setState(true);
 		}
 
 		@Override
 		public void onClick(ClickEvent event) {
-			if (event.getSource() == clarifyButton) {
-				return;
-			} else if (event.getSource() == proButton) {
+			if (event.getSource() == proButton) {
 				addArgument("Argument For");
 
 			} else if (event.getSource() == conButton) {
@@ -85,11 +96,96 @@ public class WidgetExperiments implements EntryPoint {
 		}
 
 		public void addArgument(String labelText) {
-			TreeItem labelTreeItem = new TreeItem( labelText );
-			labelTreeItem.addItem(new Proposition());
-			this.addItem( labelTreeItem );
-			labelTreeItem.setState(true);
+			TreeItem argumentTreeItem = new TreeItem(labelText);
+			argumentTreeItem.addItem(new Proposition(argumentTreeItem));
+			this.addItem(argumentTreeItem);
+			argumentTreeItem.setState(true);
+			this.setState(true);
 		}
+
+		@Override
+		public void onKeyPress(KeyPressEvent event) {
+			char charCode = event.getCharCode();
+			Object source = event.getSource();
+			String textAreaContent = textArea.getText();
+			if (source == textArea && (charCode == '\n' || charCode == '\r')
+					&& parentArgument != null) {
+				Proposition newProposition = new Proposition(parentArgument);
+
+				/*
+				 * can't figure out how to insert an item at a specific point
+				 * (instead items just get inserted as the last of the current
+				 * TreeItem's children). So, instead, I'm removing all
+				 * subsequent TreeItem children, then adding the new TreeItem
+				 * (the new proposition) and then adding back all the subsequent
+				 * tree items!
+				 */
+
+				// first remove subsequent children
+				int treePosition = parentArgument.getChildIndex(this);
+				Queue<TreeItem> removeQueue = new LinkedList<TreeItem>();
+				TreeItem currentItem;
+				while ((currentItem = parentArgument.getChild(treePosition + 1)) != null) {
+					removeQueue.add(currentItem);
+					parentArgument.removeItem(currentItem);
+				}
+
+				// then add the new one
+				parentArgument.addItem(newProposition);
+
+				// then add back the rest
+				while (!removeQueue.isEmpty()) {
+					TreeItem toRemove = removeQueue.poll();
+					parentArgument.addItem(toRemove);
+				}
+
+				newProposition.textArea.setFocus(true);
+			} else if (source == textArea && (charCode == '\b')
+					&& parentArgument != null && textAreaContent.equals("")) {
+				if (parentArgument.getChildCount() > 1) {
+					parentArgument.removeItem(this);
+				} else {
+					parentArgument.remove();
+				}
+			}
+		}
+
+		@Override
+		public void onFocus(FocusEvent event) {
+			Object source = event.getSource();
+			if( source == textArea ){
+				if( lastPropositionWithFocus != this ){
+					lastPropositionWithFocus.proButton.setVisible(false);
+					lastPropositionWithFocus.conButton.setVisible(false);
+					lastPropositionWithFocus = this;
+				}
+				proButton.setVisible(true);
+				conButton.setVisible(true);
+			}
+		}
+			/*
+			if( source == proButton ||
+					event.getSource() == conButton ){
+				buttonHasFocus = true;
+			} else if (source == textArea ){
+				proButton.setVisible(true);
+				conButton.setVisible(true);
+			}
+		}*/
+/*
+		@Override
+		public void onBlur(BlurEvent event) {
+			Object source = event.getSource();
+			if( source == proButton ||
+					event.getSource() == conButton ){
+				buttonHasFocus = false;
+			} else if (source == textArea &&
+					buttonHasFocus == false ){
+				proButton.setVisible(false);
+				conButton.setVisible(false);
+			}
+		}*/
+
 	}
 
 	private static class TextAreaSloppyGrow extends TextArea {
